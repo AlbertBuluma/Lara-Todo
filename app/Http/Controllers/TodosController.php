34551f29ca636fb122d210b40f3bbcffceef18d3
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-02 17:24:47 +0300

    Create edit and update controller methods

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -40,4 +40,27 @@ class TodosController extends Controller
         $todo->save();
         return redirect('/todos');
     }
+
+    public function edit($todoId){
+
+        $todo = Todo::find($todoId);
+        return view('todos.edit', compact('todo'));
+    }
+
+    public function update($todoId){
+        $this->validate(request(),[
+            'name' => 'required|min:6|max:6',
+            'description' => 'required'
+        ]);
+
+        $data = request()->all();
+
+        $todo = Todo::find($todoId);
+
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->save();
+        return redirect('/todos');
+
+    }
 }
